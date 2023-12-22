eee9f356eae76e0d0080d2321e4332f08c4f7388
Tuxacard <horvath.zoltan8181@gmail.com>
2023-12-22 19:22:50 +0100

    Added btrfs support

--- 1-install.sh
+++ 1-install.sh
@@ -16,7 +16,8 @@ echo "-----------------------------------------------------"
 echo "Take care the Disk layout first !"
 echo "-----------------------------------------------------"
 echo ""
-echo "Warning: Run this script at your own risk."
+echo " !! Warning: Run this script at your own risk. !!"
+echo ""
 read -p "Find the 'Any key' and please press it'" c
 # # Maybe it's required to install the current archlinux keyring
 # if the installation of git fails. Uncomment if needed.
@@ -42,6 +43,9 @@ read -p "Enter the name of the ROOT partition (eg. sda2): " sda2
 read -p "Enter the name of the HOME partition (eg. sda3): " sda3
 # read -p "Enter the name of the VM partition (keep it empty if not required): " sda3
 
+# ------------------------------------------------------
+# In case of ext4
+# ------------------------------------------------------
 
 # ------------------------------------------------------
 # Format partitions
@@ -50,12 +54,45 @@ mkfs.fat -F 32 /dev/$sda1
 mkfs.ext4 -L Arch-Root /dev/$sda2
 mkfs.ext4 -L Home /dev/$sda3
 #-------------------------------------------------------
-# Mounting partitions
+# Mount points for ext4
 # ------------------------------------------------------
 mount -o defaults,noatime /dev/$sda2 /mnt
 mkdir -p /mnt/{boot/efi,home}
 mount -o defaults,noatime /dev/$sda3 /mnt/home
 mount -o defaults,noatime /dev/$sda1 /mnt/boot/efi
+
+# ------------------------------------------------------
+# In case of BTRFS
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Format partitions
+# ------------------------------------------------------
+#mkfs.fat -F 32 /dev/$sda1;
+#mkfs.btrfs -f /dev/$sda2
+# mkfs.btrfs -f /dev/$sda3
+# ------------------------------------------------------
+# Mount points for btrfs
+# ------------------------------------------------------
+#mount /dev/$sda2 /mnt
+#btrfs su cr /mnt/@
+#btrfs su cr /mnt/@cache
+#btrfs su cr /mnt/@home
+#btrfs su cr /mnt/@snapshots
+#btrfs su cr /mnt/@log
+#umount /mnt
+
+#mount -o compress=zstd:1,noatime,subvol=@ /dev/$sda2 /mnt
+#mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log}}
+#mount -o compress=zstd:3,noatime,subvol=@cache /dev/$sda2 /mnt/var/cache
+#mount -o compress=zstd:3,noatime,subvol=@home /dev/$sda2 /mnt/home
+#mount -o compress=zstd:3,noatime,subvol=@log /dev/$sda2 /mnt/var/log
+#mount -o compress=zstd:3,noatime,subvol=@snapshots /dev/$sda2 /mnt/.snapshots
+
+#mount /dev/$sda1 /mnt/boot/efi
+# mkdir /mnt/vm
+# mount /dev/$sda3 /mnt/vm
+
 # ------------------------------------------------------
 # Install base packages
 # ------------------------------------------------------
